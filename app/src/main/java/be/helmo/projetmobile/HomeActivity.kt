3fce98a0e27a7445b663c50d219319cc239da7a9
rubenyildiz <r.yildiz@student.helmo.be>
2024-04-16 20:26:29 +0200

    bottom navbar

--- app/src/main/java/be/helmo/projetmobile/HomeActivity.kt
+++ app/src/main/java/be/helmo/projetmobile/HomeActivity.kt
@@ -3,6 +3,8 @@ package be.helmo.projetmobile
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import be.helmo.projetmobile.databinding.ActivityHomeBinding
 
 class HomeActivity : ComponentActivity() {
@@ -13,5 +15,6 @@ class HomeActivity : ComponentActivity() {
 
         val binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 }
